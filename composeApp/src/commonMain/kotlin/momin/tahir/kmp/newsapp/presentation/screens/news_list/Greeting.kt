56e871a72b151dd21545f26785f88e731070332b
Momin Tahir <momintahir7@gmail.com>
2024-01-02 19:56:37 +0500

    remove Greeting.kt file

--- composeApp/src/commonMain/kotlin/momin/tahir/kmp/newsapp/presentation/screens/news_list/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package momin.tahir.kmp.newsapp.presentation.screens.news_list
-
-class Greeting {
-//    private val platform = getPlatform()
-
-//    fun greet(): String {
-//        return "Hello, ${platform.name}!"
-//    }
-}
