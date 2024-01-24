2a5a05c018b3248ad7a4b6ee8382b222e4775b29
Momin Tahir <momintahir7@gmail.com>
2024-01-25 00:00:01 +0500

    styling the text

--- composeApp/src/commonMain/kotlin/momin/tahir/kmp/newsapp/presentation/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/momin/tahir/kmp/newsapp/presentation/screens/home/HomeScreen.kt
@@ -143,56 +143,7 @@ class HomeScreen : Screen {
                 }
             }
         }
-
     }
-
-    data class Destination(
-        val location: String,
-        val description: String,
-    )
-
-    val destinations = listOf(
-        Destination(
-            "Bali",
-            "Known for its beautiful beaches, stunning temples, and lush greenery, Bali is a tropical paradise that offers a unique blend of natural beauty and cultural experiences. It's an ideal destination for those seeking a relaxing vacation filled with yoga, spa treatments, and sunsets."
-        ),
-        Destination(
-            "Santorini",
-            "A picturesque island in the Aegean Sea, Santorini is famous for its white-washed buildings, blue-domed churches, and stunning sunsets. It's a perfect destination for honeymooners or those looking for a romantic getaway."
-        ),
-        Destination(
-            "Tokyo",
-            "A bustling city that seamlessly blends tradition and modernity, Tokyo offers an exciting vacation experience that's both vibrant and unique. From traditional Japanese gardens and temples to modern shopping districts and futuristic technology, there's something for everyone in Tokyo."
-        ),
-        Destination(
-            "Machu Picchu",
-            "A UNESCO World Heritage Site, Machu Picchu is an ancient Incan citadel situated high in the Andes Mountains. It's a destination for adventure seekers, as hiking the Inca Trail is the only way to reach the site."
-        ),
-        Destination(
-            "Banf",
-            "A stunning national park in the Canadian Rockies, Banff is a year-round destination that offers scenic vistas, crystal-clear lakes, and abundant wildlife. It's an ideal spot for nature lovers, hikers, and skiers."
-        ),
-        Destination(
-            "Marrakech",
-            "A vibrant city known for its bustling markets, colorful architecture, and rich cultural heritage, Marrakech is an exotic destination that offers a unique blend of ancient and modern experiences."
-        ),
-        Destination(
-            "The Maldives",
-            "An island paradise located in the Indian Ocean, the Maldives is known for its turquoise waters, white sandy beaches, and overwater bungalows. It's an ideal destination for those seeking a luxurious and romantic getaway."
-        ),
-        Destination(
-            "Sydney",
-            "A cosmopolitan city situated on a beautiful harbor, Sydney is a destination that offers both natural beauty and urban sophistication. It's an ideal spot for beachgoers, foodies, and culture lovers."
-        ),
-        Destination(
-            "Maui",
-            "A tropical island paradise in the Pacific Ocean, Maui is known for its beautiful beaches, scenic drives, and outdoor activities such as surfing, snorkeling, and hiking. It's an ideal destination for those seeking a laid-back vacation filled with sunshine and relaxation."
-        ),
-        Destination(
-            "Venice",
-            "A romantic city built on a series of canals, Venice is known for its stunning architecture, art, and history. It's an ideal destination for culture lovers, foodies, and those seeking a romantic getaway."
-        ),
-    )
 
     @Composable
     fun Failure(message: String) {
@@ -261,12 +212,16 @@ class HomeScreen : Screen {
                 Text(
                     text = article.title,
                     modifier = Modifier,
-                    maxLines = 1
+                    maxLines = 1,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
                 )
                 Text(
                     text = article.description.orEmpty(),
                     modifier = Modifier,
-                    maxLines = 2
+                    maxLines = 2,
+                    fontSize = 12.sp,
+                    color = Color.Black.copy(alpha = 0.6f)
                 )
 //                Button(content = {
 //                    Text("Save article")
